ef6f2fe6ea8e479114df183999b73d32d1eaa565
lucasamonrc <lucasamonrc@gmail.com>
2021-12-09 23:26:28 -0700

    implement question actions

--- src/pages/questions/[id]/index.jsx
+++ src/pages/questions/[id]/index.jsx
@@ -1,20 +1,50 @@
 import Head from "next/head";
+import { useState } from "react";
 import { getSession } from "next-auth/client";
 
 import { QuestionHeader } from "../../../components/QuestionHeader";
 import { ActionBar } from "../../../components/ActionBar";
 import { Reply } from "../../../components/Reply";
 import { supabase } from "../../../services/supabase";
+import { useRouter } from "next/router";
 
 export default function Home({ session, question }) {
+  const router = useRouter();
+  const [open, setOpen] = useState(false)
+
+  async function editQuestion() {
+    router.push(router.asPath + '/edit')
+  }
+
+  async function toggleQuestion() {
+    await supabase
+      .from('questions').update({ answered: !question.answered })
+      .match({ id: question.id });
+
+    router.reload();
+  }
+  
+  async function deleteQuestion() {
+    await supabase.from('questions').delete().match({ id: question.id });
+    router.push('/my-questions');
+  }
+
   return (
     <>
       <Head>
         <title>{question.title}</title>
       </Head>
       <div className="bg-gray-50 min-h-screen w-full max-w-md mx-auto flex flex-col">
-        <QuestionHeader question={question} user={session.user} />
-        <main className="mt-8 flex-grow mb-32">
+        <QuestionHeader
+          question={question}
+          user={session.user}
+          open={open}
+          toggleMenu={setOpen}
+          editQuestion={editQuestion}
+          toggleQuestion={toggleQuestion}
+          deleteQuestion={deleteQuestion}
+        />
+        <main className="mt-8 flex-grow mb-32" onClick={() => setOpen(false)}>
           {question.replies.map((reply, i) => (
             <Reply key={i} reply={reply} />
           ))}
