7ac571df44be25341633093675fc63717bc730fe
Ricardo Leite <ricardo.leitejr95@gmail.com>
2021-12-14 13:04:56 -0700

    Like button work

--- src/components/Reply.jsx
+++ src/components/Reply.jsx
@@ -1,6 +1,24 @@
+import { useRouter } from "next/dist/client/router";
 import { FiThumbsDown, FiThumbsUp } from "react-icons/fi";
+import { supabase } from "../services/supabase";
 
 export function Reply({ reply, user }) {
+
+  const router = useRouter()
+
+
+  //  if reply.user.id == user.id {show button} 
+
+  const incrementLike = async () => {
+      await supabase.from('replies').update({likes: reply.likes + 1 }).match({id: reply.id})
+      router.reload()
+  }
+
+  const decrementLike = async () => {
+      await supabase.from('replies').update({likes: reply.dislikes - 1 }).match({id: reply.id})
+      router.reload()
+  }
+
   return (
     <article className="bg-white p-4 shadow-lg mb-4">
       <section className="mb-4 flex justify-between items-center">
@@ -9,12 +27,16 @@ export function Reply({ reply, user }) {
           <strong>{reply.user.name}</strong>
         </div>
         <div className="flex gap-3 items-center">
-          <button className="flex gap-1 items-center">
-            {reply.likes} <FiThumbsUp />
-          </button>
-          <button className="flex gap-1 items-center">
-            {reply.dislikes} <FiThumbsDown />
-          </button>
+        <div>
+            <button className="flex gap-1 items-center" onClick={incrementLike}> 
+              {reply.likes} <FiThumbsUp color={reply.likes > 0 ? "#0000FF" : ""}/>
+            </button>
+            <button className="flex gap-1 items-center" onClick={decrementLike}>
+              {reply.dislikes} <FiThumbsDown color={reply.likes < 0 ? "#FF0000" : ""}/>
+            </button>
+            {reply.user.id === user.id && <button className="sm:container flex gap-4"  >Delete</button>}
+          </div>
+          
         </div>
       </section>
       <p>{reply.body}</p>
